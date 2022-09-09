ec307b875aa9db12a16e78fb3ecb0b436d924e70
Peter Lee <peter.lee@theaccessgroup.com>
2022-09-09 14:42:19 +1000

    add v2 and depend on v1

---
+++ GHPT/GHPT.V2/GHPT.V2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GHPT;
+
+namespace GHPT.V2
+{
+    
+    public class GhptV2
+    {
+        private string _name;
+
+        public GhptV2()
+        {
+            this._name = $"Github Package Test Nuget V2 {Ghpt.Name}";
+        }
+    }
+}
